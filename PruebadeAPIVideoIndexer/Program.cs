c9ba790dd25309391d442b72789518407136911a
serchfr <serfr890@outlook.es>
2019-04-23 11:41:18 -0500

    Se borran Comentarios

--- PruebadeAPIVideoIndexer/Program.cs
+++ PruebadeAPIVideoIndexer/Program.cs
@@ -12,63 +12,16 @@ namespace PruebadeAPIVideoIndexer
 {
     class Program
     {
-        private static string OCP_APIM_SUBCRIPTION_KEY = "Ocp-Apim-Subscription-Key";
-        private static string SUBSCRIPTION_KEY = "9910bef53a484b1ba9eb9961f8815d64";
-        private static string LOCATION = "trial";
-        private static string ACCOUNTID = "bc129b9e-ee67-4686-ba40-76f051a5911f";
-        private static string accessToken;
+        //private static string accessToken;
         static void Main(string[] args)
         {
             VideoIndexerMethods videoIndexerMethods = new VideoIndexerMethods();
-            
             var result = Task.Run(() => videoIndexerMethods.GetAccountAccesTokenAsync());
             result.Wait();
-            accessToken = result.Result.Trim('\"');
+            var accessToken = result.Result.Trim('\"');
             videoIndexerMethods.SearchVideos(accessToken);
             Console.WriteLine("Hit ENTER to exit...");
             Console.ReadKey();
         }
-        /*static async Task<string> GetAccountAccesTokenAsync()
-        {
-            var client = new HttpClient();
-            var queryString = HttpUtility.ParseQueryString(string.Empty);
-            client.DefaultRequestHeaders.Add(OCP_APIM_SUBCRIPTION_KEY, SUBSCRIPTION_KEY);
-            var uri = "https://api.videoindexer.ai/auth/" + LOCATION + "/Accounts/" + ACCOUNTID + "/AccessToken";// + queryString;
-            var responseMessage = await client.GetAsync(uri);
-            return await responseMessage.Content.ReadAsStringAsync();
-        }
-
-        private static async void SearchVideos()
-        {
-            Console.WriteLine(accessToken);
-            using (var client = new HttpClient())
-            {
-                var queryString = HttpUtility.ParseQueryString(string.Empty);
-                // Request parameters
-                string text = "valor";
-                queryString["query"] = text;
-                queryString["pageSize"] = "25";
-                queryString["skip"] = "0";
-                queryString["accessToken"] = accessToken;
-                var uri = "https://api.videoindexer.ai/" + LOCATION + "/Accounts/" + ACCOUNTID + "/Videos/Search?" + queryString;
-                var responseMessage = await client.GetAsync(uri);
-                var result = await responseMessage.Content.ReadAsStringAsync();
-                var jsonResult = JsonConvert.DeserializeObject<VideoSearchResult>(result);
-
-                queryString.Clear();
-                queryString["format"] = "Jpeg";
-                queryString["accessToken"] = accessToken;
-                Console.WriteLine("Se encontró '{0}' en los siguientes videos: ", text);
-                foreach (var item in jsonResult.results)
-                {
-                    Console.WriteLine("Nombre: " + item.name);
-                    Console.WriteLine("Tiempo: " + item.searchMatches[0].startTime);
-                    Console.WriteLine("URL: " + "https://www.videoindexer.ai/accounts/bc129b9e-ee67-4686-ba40-76f051a5911f/videos/" + item.id + "/");
-                    Console.WriteLine("Thumbnaill: " + "https://api.videoindexer.ai/" + LOCATION + "/Accounts/"+ ACCOUNTID + "/Videos/"+ item.id + "/Thumbnails/"+ item.thumbnailId +"?" + queryString);
-                }
-            }
-        }*/
     }
-
-    
 }
